b67d104120d831a4cd4259a944532bbdbfef7a26
Srujal Kachhela <srujal.k@gmail.com>
2019-03-28 22:11:02 +0530

    Removed Node Modules Folder

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var movie = require('../models/movies');
-var movies_list = [];
-
-/* GET home page. */
-exports.index = function(req, res){
-  res.render('index', {
-    title: 'MovieBooker'
-  });
-};
